2bd594347c4f7585a29532e185fd7bf0752633cb
AzwadFawadHasan <azwadfawadhasan@gmail.com>
2026-01-28 18:15:54 +0600

    refactore: resturcture project files

--- simple_demo.py
+++ simple_demo.py
@@ -7,7 +7,5 @@ from screenshot_scanner import ScreenshotScanner
 
 # That's it! Just 3 lines!
 scanner = ScreenshotScanner()
-# result = scanner.process(f"C:/Users/dellG15/Documents/ScreenshotScanner/test_data/False/20260115_153409.jpg")
 result = scanner.process(f"test_data/False/20260115_153409.jpg")
-result = scanner.process(f"test_data/True/Screenshot 2026-01-20 162653.jpg")
 print(result)
